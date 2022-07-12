6965a34226ad8f42725d0a3dbdbda128786a0170
Alex Nguyen <hoangminh98@gmail.com>
2022-07-11 23:24:17 -0700

    Switch to `ListView` for `MemberListView`

--- team/views/list_view.py
+++ team/views/list_view.py
@@ -1,13 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from team.models import Member
 
 
-class MemberListView(TemplateView):
+class MemberListView(ListView):
     template_name = "team/list.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["members"] = Member.objects.all()
-        context["member_count"] = Member.objects.count()
-        return context
+    model = Member
+    context_object_name = "members"
